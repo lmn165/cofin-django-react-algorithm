86c0ad20da9d3b2bf8611c5e9f38932c0e6b307f
lmn165 <lastalking@naver.com>
2021-09-27 16:28:49 +0900

    Sept 27 Conditional Rendering

--- frontend/src/common/components/Counter.js
+++ frontend/src/common/components/Counter.js
@@ -3,19 +3,34 @@ import Badge from '@mui/material/Badge';
 import MailIcon from '@mui/icons-material/Mail';
 import { Button } from "@mui/material";
 import styled from 'styled-components'
+import Alert from '@mui/material/Alert';
+import AlertTitle from '@mui/material/AlertTitle';
+import Stack from '@mui/material/Stack';
 
 export default function Counter(){
+    const [count, setCount] = useState(0)
+
     return (<CounterDiv>
-        <Badge badgeContent={4} color="secondary">
+        {count == 0 && <Stack sx={{ width: '300px', margin: '0 auto'}} spacing={2}>
+          <Alert severity="warning">
+            <AlertTitle>Warning</AlertTitle>
+            <strong>메시지가 없습니다.</strong>
+          </Alert>
+        </Stack>}
+        <Badge badgeContent={count >= 0 ? count : setCount(0)} color="secondary">
           <MailIcon color="action" />
         </Badge>
-
-        <Button variant="outlined">
+        <br/>
+        <Button variant="outlined" onClick={() => setCount(count + 1)}>
             Add
         </Button>
+        <SpanStyle/>
+        <Button variant="outlined" onClick={() => setCount(count - 1)}>
+            Del
+        </Button>
+        <br/>
     </CounterDiv>)
 }
 
-const CounterDiv = styled.div`
-    text-align: center;
-`
+const CounterDiv = styled.div` text-align: center; `
+const SpanStyle = styled.span ` margin: 10px; `
