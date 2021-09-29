f51dc018115d892babd072da9495fdafbcf8ca40
lmn165 <lastalking@naver.com>
2021-09-29 16:35:34 +0900

    Sept 29 update, delete in Redux store

--- frontend/src/common/components/TodoList.js
+++ frontend/src/common/components/TodoList.js
@@ -1,13 +1,25 @@
 import React from "react";
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
+import { toggleTodoAction, deleteTodoAction } from "reducers/todo.reducer";
 
 export default function TodoList() {
     const todos = useSelector( state => state.todoReducer.todos )
-
+    const dispatch = useDispatch()
+    const toggleTodo = id => dispatch(toggleTodoAction(id))
+    const deleteTodo = id => dispatch(deleteTodoAction(id))
     return (
     <>
         {todos.length === 0 && (<h1>지금은 등록된 화면이 없습니다.</h1>) }
-        {todos.length !== 0
-            && (<h1>{todos.length} 개의 할일 목록이 있습니다.</h1>)}
+        {todos.length !== 0 && (<h1>{todos.length} 개의 할일 목록이 있습니다.</h1>)}
+
+        {todos.length !== 0 && todos.map(
+            todo => (<div key={todo.id}>
+                <input type='checkbox' checked={todo.complete} onChange={toggleTodo.bind(null, todo.id)}/>
+                {todo.complete ?
+                <span style={{textDecoration: 'line-through'}}>{todo.name}</span>
+                : <span>{todo.name}</span>}
+                <button onClick={deleteTodo.bind(null, todo.id)}>X</button>
+            </div>)
+        )}
     </>)
 }
--- frontend/src/reducers/todo.reducer.js
+++ frontend/src/reducers/todo.reducer.js
@@ -1,8 +1,13 @@
 const initialState = {todos:[], todo:{}}
 export const addTodoAction = todo => ({type: "ADD_TODO", payload: todo})
+export const toggleTodoAction = todoid => ({type: "TOGGLE_TODO", payload: todoid})
+export const deleteTodoAction = todoid => ({type: "DELETE_TODO", payload: todoid})
 const todoReducer = (state = initialState, action) => {
     switch(action.type){
         case 'ADD_TODO': return {...state, todos:[...state.todos, action.payload]}
+        case 'TOGGLE_TODO': return {...state, todos:state.todos.map(todo => (todo.id === action.payload)
+                                                                    ? {...todo, complete: !todo.complete} : todo)}
+        case 'DELETE_TODO': return {...state, todos:state.todos.filter(todo => todo.id != action.payload)}
         default : return state
     }
 }
