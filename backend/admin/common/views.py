f893afe19ad434111fbb6152453910b0b546e3bb
lmn165 <lastalking@naver.com>
2021-10-01 10:42:09 +0900

    Oct 01 Change the connection method.

--- backend/admin/common/views.py
+++ backend/admin/common/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
-from rest_framework.response import Response
-from rest_framework.views import APIView
+from django.http import JsonResponse
+from rest_framework.decorators import api_view, parser_classes
+from rest_framework.parsers import JSONParser
 
 
-class Connection(APIView):
-    def get(self, request):
-        return Response({'connection': 'SUCCESS'})
+@api_view(['GET'])
+@parser_classes([JSONParser])
+def connection(request):
+    return JsonResponse({'connection': 'SUCCESS'})
--- backend/admin/urls.py
+++ backend/admin/urls.py
@@ -14,10 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from admin.common.views import Connection
+from django.urls import path, include
+
 urlpatterns = [
     # path('admin/', admin.site.urls),
-    path('api/connect', Connection.as_view()),
+    path('api/connect', include('admin.common.urls')),
+    path('api/users/', include('admin.member.urls')),
 ]
 
