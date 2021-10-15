a4b7b69168b8384ac41c3d97b1f5fd71bc37b6c2
lmn165 <lastalking@naver.com>
2021-10-15 14:34:14 +0900

    Oct 15 Visualize Crime Model

--- backend/admin/crime/models.py
+++ backend/admin/crime/models.py
@@ -1,10 +1,13 @@
+import csv
 from datetime import datetime
 
 import pandas as pd
+from sklearn import preprocessing
 
 from admin.common.models import ValueObject, Printer, Reader
 from icecream import ic
 import numpy as np
+import folium
 
 
 class Crime():
@@ -23,14 +26,14 @@ class Crime():
         reader = Reader()
         printer = Printer()
         vo.context = 'admin/crime/data/'
-        crime_columns = ['살인발생', '강도발생', '강간발생', '절도발생', '폭력발생']  # Nominal
-        arrest_columns = ['살인검거', '강도검거', '강간검거', '절도검거', '폭력검거']  # Nominal
+        crime_columns = ['살인 발생', '강도 발생', '강간 발생', '절도 발생', '폭력 발생']  # Nominal
+        arrest_columns = ['살인 검거', '강도 검거', '강간 검거', '절도 검거', '폭력 검거']  # Nominal
         arrest_rate_columns = ['살인검거율', '강도검거율', '강간검거율', '절도검거율', '폭력검거율']  # Ratio
         print('[1] crime_df 생성 ')
         vo.fname = 'crime_in_Seoul'
         crime_df = reader.csv(reader.new_file(vo))
         print('[2] crime_df 에 경찰서위치 추가 ')
-        # self.crime_police(crime_df, reader, vo)
+        # self.crime_police(crime_df, reader, vo)  # ::: GOOGLE MAP
         vo.fname = 'new_data/crime_police'
         crime_df = reader.csv(reader.new_file(vo))
         print('[3] cctv_df CREATION ')
@@ -46,16 +49,76 @@ class Crime():
         cctv_pop_df = pd.merge(cctv_df, pop_df)
         cctv_pop_corr = cctv_pop_df.corr()
         print(cctv_pop_corr)
+        '''
+        CCTV와 상관계수: 한국인 0.3, 외국인 0, 고령자 0.2   
+        '''
         crime_df = crime_df.groupby('구별').sum()
-        crime_df['총 범죄 수'] = crime_df.loc[:, crime_df.columns.str.contains(' 발생$', case=False, regex=True)].sum(axis=1)
-        crime_df['총 검거 수'] = crime_df.loc[:, crime_df.columns.str.contains(' 검거$', case=False, regex=True)].sum(axis=1)
-        crime_df['총 검거율'] = crime_df['총 검거 수'] / crime_df['총 범죄 수'] * 100
-        cctv_crime_df = pd.merge(cctv_df.loc[:, ['구별', '소계']], crime_df.loc[:, '총 범죄 수':'총 검거율'], on='구별')
+        crime_df['총범죄수'] = crime_df.loc[:, crime_df.columns.str.contains(' 발생$', case=False, regex=True)].sum(axis=1)
+        crime_df['총검거수'] = crime_df.loc[:, crime_df.columns.str.contains(' 검거$', case=False, regex=True)].sum(axis=1)
+        crime_df['총검거율'] = crime_df['총검거수'] / crime_df['총범죄수'] * 100
+        cctv_crime_df = pd.merge(cctv_df.loc[:, ['구별', '소계']], crime_df.loc[:, '총범죄수':'총검거율'], on='구별')
         cctv_crime_df.rename(columns={"소계": "CCTV총합"}, inplace=True)
         print(cctv_crime_df.corr())
+        '''
+        CCTV와 상관계수: 범죄수 0.47, 검거수 0.52 
+        '''
         print('[6] police_df CREATION ')
         police_df = pd.pivot_table(crime_df, index='구별', aggfunc=np.sum)
         print(police_df)
+        print(f'경찰서DF 컬럼: {police_df.columns}')
+        '''
+         ['강간 검거', '강간 발생', '강도 검거', '강도 발생', '살인 검거', '살인 발생','절도 검거', 
+         '절도 발생', '총검거수', '총검거율', '총범죄수', '폭력 검거', '폭력 발생']
+        '''
+        for i, j in enumerate(crime_columns):
+            police_df[arrest_rate_columns[i]] = \
+                (police_df[arrest_columns[i]].astype(int) / police_df[j].astype(int)) * 100
+
+        police_df.drop(columns={'살인 검거', '강도 검거', '강간 검거', '절도 검거', '폭력 검거'}, axis=1, inplace=True)
+        for i in arrest_rate_columns:
+            police_df.loc[police_df[i] > 100, 1] = 100  # 데이터값 기간이 1년을 넘긴 경우가 있어서 100을 max 로 지정
+        vals = ['살인', '강도', '강간', '절도', '폭력']
+        keys = [f'{i} 발생' for i in vals]
+        columns = dict(zip(keys, vals))
+        police_df.rename(columns=columns, inplace=True)
+        x = police_df[arrest_rate_columns].values
+        # from sklearn import preprocessing 추가
+        min_max_scalar = preprocessing.MinMaxScaler()
+        # 스케일링은 선형변환을 적용하여 전체 자료의 분포를 평균 0, 분산 1이 되도록 만드는 과정
+        x_scaled = min_max_scalar.fit_transform(x.astype(float))
+        # 정규화 normalization (가우스 분포)
+        # 1. 빅데이터를 처리하면서 데이터의 범위(도메인)를 일치시킨다.
+        # 2. 분포(스케일)를 유사하게 만든다
+        print('[7] police_norm_df CREATION')
+        police_norm_df = pd.DataFrame(x_scaled, columns=crime_columns, index=police_df.index)
+        police_norm_df[arrest_rate_columns] = police_df[arrest_rate_columns]
+        police_norm_df['범죄'] = np.sum(police_norm_df[crime_columns], axis=1)
+        police_norm_df['검거'] = np.sum(police_norm_df[arrest_rate_columns], axis=1)
+        police_norm_df.to_csv(vo.context + 'new_data/police_norm.csv', sep=',', encoding='UTF-8')
+        police_norm_df = pd.read_csv(vo.context + 'new_data/police_norm.csv')
+        print(police_norm_df.columns)
+        temp = crime_df[arrest_columns] / crime_df[arrest_columns].max()
+        crime_df['검거'] = np.sum(temp, axis=1)
+        crime_police_tuple = tuple(zip(police_norm_df['구별'], police_norm_df['범죄']))
+
+        print('[8] folium CREATION')
+        vo.fname = 'geo_simple'
+        state_geo = reader.json(reader.new_file(vo))
+        m = folium.Map(location=[37.5642135, 127.0016985], zoom_start=12)  # Seoul
+
+        folium.Choropleth(
+            geo_data=state_geo,
+            name="choropleth",
+            data=crime_police_tuple,
+            columns=["Gu", "Crime Rate"],
+            key_on="feature.id",
+            fill_color="YlGn",
+            fill_opacity=0.7,
+            line_opacity=0.2,
+            legend_name="Unemployment Rate (%)",
+        ).add_to(m)
+        folium.LayerControl().add_to(m)
+        m.save(vo.context+'new_data/folium.html')
 
     def crime_police(self, crime_df, reader, vo):
         station_names = []
@@ -71,6 +134,15 @@ class Crime():
             temp_loc = temp[0].get('geometry')
             station_lats.append(temp_loc['location']['lat'])
             station_lngs.append(temp_loc['location']['lng'])
+
+        dt = dict(zip(station_lats, station_lngs))
+        with open((vo.context+'test.csv'), 'w', encoding='UTF-8') as f:
+            w = csv.writer(f)
+            w.writerow(dt.keys())
+            w.writerow(dt.values())
+
+        crime_df['lat'] = station_lats
+        crime_df['lng'] = station_lngs
         gu_names = []
         for name in station_addrs:
             temp = name.split()
@@ -78,4 +150,5 @@ class Crime():
             gu_names.append(gu_name)
         crime_df['구별'] = gu_names
         print(crime_df[crime_df['관서명'] == '혜화서'])
+        crime_df.loc[19, '구별'] = '강서구'
         crime_df.to_csv(vo.context + 'new_data/crime_police.csv')
